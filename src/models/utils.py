a3145e2c00e2b6e52f62ef8614becc8741d0c6e9
Cris Bettis <cristopher.bettis@lazarus.enterprises>
2025-05-23 11:47:42 -0400

    Update batching typing
    Allows batching to take in an array of file paths.

--- src/models/utils.py
+++ src/models/utils.py
@@ -1,20 +1,22 @@
+from typing import Union
+
 from models.apis import Rikai2, RikaiExtract, Riky2
 from models.batching import Batcher
 
 
-def query_rikai2(file_path, prompt: str):
+def query_rikai2(file_path: Union[str, list], prompt: str):
     model_api = Rikai2()
     batch = Batcher(model_api, file_path, prompt)
     batch.run()
 
 
-def query_riky2(file_path, prompt: str):
+def query_riky2(file_path: Union[str, list], prompt: str):
     model_api = Riky2()
     batch = Batcher(model_api, file_path, prompt)
     batch.run()
 
 
-def query_rikai_extract(file_path, prompt: str):
+def query_rikai_extract(file_path: Union[str, list], prompt: str):
     model_api = RikaiExtract()
     batch = Batcher(model_api, file_path, prompt)
     batch.run()
