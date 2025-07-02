2bfaeb161be9b61d65b017a577e555dc20b8e6d3
Cris Bettis <cristopher.bettis@lazarus.enterprises>
2025-07-02 11:49:09 -0400

    Add additional unit tests (#28)
    * Add additional unit tests
    
    XLS -> CSV creator

--- tests/transformations/pdf/test_utils.py
+++ tests/transformations/pdf/test_utils.py
@@ -2,7 +2,7 @@ import tempfile
 
 import pytest
 
-from lazarus_implementation_tools.file_system.utils import in_working
+from lazarus_implementation_tools.file_system.utils import get_extension, in_working
 from lazarus_implementation_tools.transformations.pdf.utils import (
     convert_to_pdf,
     get_number_of_pages,
@@ -11,7 +11,7 @@ from lazarus_implementation_tools.transformations.pdf.utils import (
 test_files = [
     ("word_processing/Sherlock_Holmes_Fan_Club_Newsletter.docx", 2),
     ("powerpoint/Sherlock_Holmes_Fan_Club_Newsletter.pptx", 3),
-    ("spreadsheets/sherlock_holmes_fan_club.xlsx", 1),
+    ("spreadsheets/sherlock_holmes_fan_club.xlsx", 2),
     ("message/Sherlock_Holmes_Fan_Club.msg", 7),
     ("images/Sherlock_Holmes_Fan_Club_Flyer.png", 1),
 ]
@@ -25,4 +25,5 @@ def test_convert_to_pdf(file_path, page_count):
         actual_file = converted_files.pop(0)
         # Get page count confirms it opens as a pdf
         # and gets a sense if all the pages are there.
+        assert get_extension(actual_file) == "pdf"
         assert get_number_of_pages(actual_file) == page_count
